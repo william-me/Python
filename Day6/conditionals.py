caba83f98a506786a5b194fbf6f7def8137043f7
william-me <willygichohim@gmail.com>
2023-12-08 19:23:59 +0300

    Day

---
+++ Day6/conditionals.py
@@ -0,0 +1,23 @@
+#if statement
+x = 10
+if x > 5:
+    print("x is greater than 5")
+    # Code to execute if the condition is True
+    
+
+#Elif statement    
+x = 10
+if x > 15:
+    print("x is greater than 15")
+elif x > 5:
+    print("x is greater than 5 but not greater than 15")
+else:
+    print("x is not greater than 5")
+ 
+    
+#Else statement
+x = 3
+if x > 5:
+    print("x is greater than 5")
+else:
+    print("x is not greater than 5")
